aeb155e268667bb18ef7266562a7ed517f31c416
Noel Lopes <noel@ipg.pt>
2018-05-30 17:28:45 +0100

    Added book class (business model)

---
+++ app/src/main/java/pt/ipg/books/Book.java
@@ -0,0 +1,40 @@
+package pt.ipg.books;
+
+public class Book {
+    private int id;
+    private String title;
+    private double price;
+    private int idCategory;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getIdCategory() {
+        return idCategory;
+    }
+
+    public void setIdCategory(int idCategory) {
+        this.idCategory = idCategory;
+    }
+}
