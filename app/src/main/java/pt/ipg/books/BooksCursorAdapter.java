813a7073ad15dc05890f7f7b39327d667e11548f
Noel Lopes <noel@ipg.pt>
2018-06-15 12:10:25 +0100

    Finished BooksCursorAdapter

--- app/src/main/java/pt/ipg/books/BooksCursorAdapter.java
+++ app/src/main/java/pt/ipg/books/BooksCursorAdapter.java
@@ -1,18 +1,28 @@
 package pt.ipg.books;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class BooksCursorAdapter extends RecyclerView.Adapter<BooksCursorAdapter.BookViewHolder> {
     private Context context;
+    private Cursor cursor = null;
 
     public BooksCursorAdapter(Context context) {
         this.context = context;
+    }
+
+    public void refreshData(Cursor cursor) {
+        if (this.cursor != cursor) {
+            this.cursor = cursor;
+            notifyDataSetChanged();
+        }
     }
 
     /**
@@ -65,7 +75,9 @@ public class BooksCursorAdapter extends RecyclerView.Adapter<BooksCursorAdapter.
      */
     @Override
     public void onBindViewHolder(@NonNull BookViewHolder holder, int position) {
-
+        cursor.moveToPosition(position);
+        Book book = DbTableBooks.getCurrentBookFromCursor(cursor);
+        holder.setBook(book);
     }
 
     /**
@@ -75,12 +87,25 @@ public class BooksCursorAdapter extends RecyclerView.Adapter<BooksCursorAdapter.
      */
     @Override
     public int getItemCount() {
-        return 0;
+        if (cursor == null) return 0;
+
+        return cursor.getCount();
     }
 
     public class BookViewHolder extends RecyclerView.ViewHolder {
+        private TextView textViewTitle;
+        private TextView textViewPrice;
+
         public BookViewHolder(View itemView) {
             super(itemView);
+
+            textViewTitle = (TextView) itemView.findViewById(R.id.textViewTitle);
+            textViewPrice = (TextView) itemView.findViewById(R.id.textViewPrice);
+        }
+
+        public void setBook(Book book) {
+            textViewTitle.setText(book.getTitle());
+            textViewPrice.setText(String.format("%.2f", book.getPrice()) + "€");
         }
     }
 }
